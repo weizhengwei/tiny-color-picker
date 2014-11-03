7717d8b5b9927a60c59c4f9e9895bfe5ea17bb86
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2014-11-03 23:12:57 +0100

    Update tiny.cpp

--- tiny.cpp
+++ tiny.cpp
@@ -26,150 +26,132 @@ const char g_ProgramTitle[] = "Tiny Color Picker";
 
 // i could not find a better place to set the balloon tip
 const char g_BalloonMessage[] = 
-	"Press ScrollLock to copy color to Clipboard.\n"
-	"Double-click on tray icon to exit.";
+    "Press ScrollLock to copy color to Clipboard.\n"
+    "Double-click on tray icon to exit.";
 
 #include "tiny.h"
 
 // WndProc is a callback function which is executed 
 // every time some interaction happens (eg. mouse move, close, maximize)
 LRESULT CALLBACK WndProc(
-	HWND hWnd, 
-	UINT nMsg, 
-	WPARAM wParam, 
-	LPARAM lParam
-){	
-	switch (nMsg)
-	{
-		// if we are doing interaction with the tray icon
-		case WM_TRAY_DBCLK:
-			
-			// for a double-click, we simply exid the application
-			if (lParam == WM_LBUTTONDBLCLK){
-				
-				// the tray icon would stay there if 
-				// we did not call the delete function
-				Shell_NotifyIcon(NIM_DELETE, &g_TrayIconInfo);
+    HWND hWnd, 
+    UINT nMsg, 
+    WPARAM wParam, 
+    LPARAM lParam
+){  
+    switch (nMsg)
+    {
+        // if we are doing interaction with the tray icon
+        case WM_TRAY_DBCLK:
+            
+            // for a double-click, we simply exit the application
+            if (lParam == WM_LBUTTONDBLCLK){
+                
+                // the tray icon would stay there if 
+                // we did not call the delete function
+                Shell_NotifyIcon(NIM_DELETE, &g_TrayIconInfo);
 
-				// nMsg becomes WM_DESTROY
-				DestroyWindow(hWnd);
-			}
+                // nMsg becomes WM_DESTROY
+                DestroyWindow(hWnd);
+            }
 
-			// for right click, we show the balloon tip
-			else if (lParam == WM_RBUTTONDOWN){ 
+            // for right click, we show the balloon tip
+            else if (lParam == WM_RBUTTONDOWN){ 
 
-				// uFlags get an additional flag with OR, called NIF_INFO
-				// because by default (we disabled it) 
-				// the balloon tip is not shown
-				g_TrayIconInfo.uFlags |= NIF_INFO;
+                // uFlags get an additional flag with OR, called NIF_INFO
+                // because the balloon tip is not shown
+                // (we disabled it after 10 sec) 
+                g_TrayIconInfo.uFlags |= NIF_INFO;
 
-				// by sending the flag NIM_MODIFY, we refresh the tray icon
-				// to show the balloon again
-				Shell_NotifyIcon(NIM_MODIFY, &g_TrayIconInfo); 
-			}
-			break;
+                // by sending the flag NIM_MODIFY, we refresh the tray icon
+                // to show the balloon again
+                Shell_NotifyIcon(NIM_MODIFY, &g_TrayIconInfo); 
+            }
+            break;
 
-		// initializing 
-		case WM_CREATE:
+        // initializing 
+        case WM_CREATE:
 
-			// we set up the tray icon
-			// at that moment the icon only shows 
-			// a still color of the pixel under the current mouse position
-			// that means we update the animation somewhere else
-			CreateTrayIcon(
-				hWnd, 
-				&g_TrayIconInfo, 
-				g_ProgramTitle, 
-				g_BalloonMessage
-				);
+            // we set up the tray icon
+            // at that moment the icon only shows 
+            // a still color of the pixel under the current mouse position
+            // that means we update the animation somewhere else
+            CreateTrayIcon(
+                hWnd, 
+                &g_TrayIconInfo, 
+                g_ProgramTitle, 
+                g_BalloonMessage
+            );
 
-			// since WndProc is an asynchronous function 
-			// (and by that i mean if you do nothing it is not even called)
-			// we cannot rely on it to start a key-hook loop
-			// so we have to create another thread (PixelToClipboard)
-			// which will catch our keypresses
-			g_hfnPixelCapture = CreateThread(
-				NULL, 0, 
-				Thread_PixelToClipboard, 
-				&g_DataPixelCapture, 
-				0, NULL
-				);  
+            // since WndProc is an asynchronous function 
+            // (and by that i mean if you do nothing it is not even called)
+            // we cannot rely on it to start a key-hook loop
+            // so we have to create another thread (PixelToClipboard)
+            // which will catch our keypresses
+            g_hfnPixelCapture = CreateThread(
+                NULL, 0, 
+                Thread_PixelToClipboard, 
+                &g_DataPixelCapture, 
+                0, NULL
+            );
+            break;
 
-			if (g_hfnPixelCapture == NULL)
-				ExitProcess(g_DataPixelCapture);
-			break;
-
-		// after closing the application
-		// nMsg becomes WM_DESTROY so we can free memory 
-		case WM_DESTROY:
-			ExitProcess(g_DataPixelCapture);
-			CloseHandle(g_hfnPixelCapture);
-			DeleteObject(g_hbmColorIcon);
-			PostQuitMessage(0);
-			break;
-		default:
-			return DefWindowProc(hWnd, nMsg, wParam, lParam);
-	}
-	return 0;
+        // after closing the application
+        // nMsg becomes WM_DESTROY so we can free memory 
+        case WM_DESTROY:
+            // get out of everything we were in
+            ExitProcess(g_DataPixelCapture);
+            CloseHandle(g_hfnPixelCapture);
+            DeleteObject(g_hbmColorIcon);
+            PostQuitMessage(0);
+            break;
+        default:
+            // TODO: is this important?
+            return DefWindowProc(hWnd, nMsg, wParam, lParam);
+    }
+    return 0;
 }
 
 int WINAPI WinMain(
-	HINSTANCE hInstance, 
-	HINSTANCE hPrevInstance, 
-	LPSTR lpCmdLine, 
-	int nCmdShow
+    HINSTANCE hInstance, 
+    HINSTANCE hPrevInstance, 
+    LPSTR lpCmdLine, 
+    int nCmdShow
 ){
-	// the following stuff is just a basic win32 application creation 
-	// without window
+    // the following stuff is just a basic win32 application creation 
+    // without window
 
-	WNDCLASSEX wc;
-	HWND hwnd;
-	MSG Msg;
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
 
-	ZeroMemory(&wc, sizeof( WNDCLASSEX));
-	wc.cbSize        = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc   = WndProc;
-	wc.hInstance     = hInstance;
-	wc.lpszClassName = g_ProgramTitle;
+    //ZeroMemory(&wc, sizeof( WNDCLASSEX));
+    tiny_memset(&wc, 0, sizeof(WNDCLASSEX));
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = g_ProgramTitle;
 
-	if(!RegisterClassEx(&wc))
-	{
-		return MessageBox(
-			NULL, 
-			"Window Registration Failed!", 
-			"Error!", 
-			MB_ICONEXCLAMATION | MB_OK
-			);
-	}
+    RegisterClassEx(&wc);
 
-	hwnd = CreateWindowEx(
-		WS_EX_CLIENTEDGE,
-		g_ProgramTitle,
-		"Tiny Color Picker",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 256, 64,
-		NULL, NULL, hInstance, NULL);
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_ProgramTitle,
+        g_ProgramTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 256, 64,
+        NULL, NULL, hInstance, NULL
+    );
 
-	if(hwnd == NULL)
-	{
-		return MessageBox(
-			NULL, 
-			"Window Creation Failed!", 
-			"Error!", 
-			MB_ICONEXCLAMATION | MB_OK
-			);
-	}
+    // disable window by not showing the window
+    // ShowWindow(hwnd, nCmdShow);
+    // UpdateWindow(hwnd);
 
-	// disable window by not showing the window
-	// you ain't get no window t'is time
-	// ShowWindow(hwnd, nCmdShow);
-	// UpdateWindow(hwnd);
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
 
-	while(GetMessage(&Msg, NULL, 0, 0) > 0)
-	{
-		TranslateMessage(&Msg);
-		DispatchMessage(&Msg);
-	}
-
-	return Msg.wParam;
+    return Msg.wParam;
 }
